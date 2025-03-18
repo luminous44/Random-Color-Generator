907656fc360ca9869b3077cfb2ed248bb9eb632e
aklema094 <aklemasultana094@gmail.com>
2025-03-18 18:59:43 +0600

    added copied color to clipboard

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ for(let i =0; i < 32; i++){
     const colorContainer = document.createElement("div");
     colorContainer.classList.add("color-container");
     containerEl.appendChild(colorContainer);
+
 }
 
 const allColorContainer = document.querySelectorAll(".color-container");
@@ -12,12 +13,28 @@ generateColor();
 function generateColor(){
     
     allColorContainer.forEach((colorContainer) =>{
+        colorContainer.style.cursor = "pointer";
         const newColorCode = randomeColor();
         console.log(newColorCode);
         colorContainer.style.backgroundColor = "#" + newColorCode;
         colorContainer.innerText = "#" + newColorCode;
+
+        colorContainer.addEventListener("click",()=>{
+
+            navigator.clipboard.writeText("#"+newColorCode);
+             // Add transform effect when copied
+             colorContainer.style.transform = "scale(1.1)";
+             colorContainer.style.transition = "transform 0.2s ease";
+
+        // Reset the effect after 0.3s
+          setTimeout(() => {
+            colorContainer.style.transform = "scale(1)";
+        }, 300);
+          
+        });
     });
 }
+
 
 function randomeColor(){
     const chars = "0123456789abcdef";
